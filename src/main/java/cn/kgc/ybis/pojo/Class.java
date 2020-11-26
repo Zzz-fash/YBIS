d5c3594c782ddbe85c65d85fe6c515c0ee09d98d
zhangtong_glb <zhangtong_glb@kgc.com>
2020-11-26 14:19:34 +0800

    ZT pojo

---
+++ src/main/java/cn/kgc/ybis/pojo/Class.java
@@ -0,0 +1,53 @@
+package cn.kgc.ybis.pojo;
+
+public class Class {
+    private Integer cId;
+
+    private String cName;
+
+    private Integer tmId;
+
+    private Integer sId;
+
+    private Integer status;
+
+    public Integer getcId() {
+        return cId;
+    }
+
+    public void setcId(Integer cId) {
+        this.cId = cId;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public void setcName(String cName) {
+        this.cName = cName == null ? null : cName.trim();
+    }
+
+    public Integer getTmId() {
+        return tmId;
+    }
+
+    public void setTmId(Integer tmId) {
+        this.tmId = tmId;
+    }
+
+    public Integer getsId() {
+        return sId;
+    }
+
+    public void setsId(Integer sId) {
+        this.sId = sId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
